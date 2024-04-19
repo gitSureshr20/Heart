0b5ed7a3ecbe160812dd6e14cf80eab9a80d34ad
Suresh R <121588527+gitSureshr20@users.noreply.github.com>
2024-04-19 14:37:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+const grayHeart = document.querySelector(".gray-heart");
+const redHeart = document.querySelector(".red-heart");
+
+
+grayHeart.addEventListener("click",()=>{
+    redHeart.classList.add("animation");
+    grayHeart.classList.add("fill-color")
+});
+
+
+redHeart.addEventListener("click",()=>{
+    redHeart.classList.remove("animation");
+    grayHeart.classList.add("fill-color")
+});
